e350f6f07d19ee5b60188bbc23cb787f9250b544
DanikNik <sway9009>
2019-03-04 18:50:36 +0300

    project layout final

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "../../internal/app/server"
-    "fmt"
-    "os"
+	"fmt"
+	"github.com/go-park-mail-ru/2019_1_OPG_plus_2/internal/app/server"
+	"os"
 )
 
 func main() {
-    params := server.Params{Port: os.Getenv("PORT")}
-    if params.Port == "" {
-        params.Port = "8001"
-    }
+	params := server.Params{Port: os.Getenv("PORT")}
+	if params.Port == "" {
+		params.Port = "8001"
+	}
 
-    err := server.StartApp(params)
-    if err != nil {
-        fmt.Println()
-    }
+	err := server.StartApp(params)
+	if err != nil {
+		fmt.Println()
+	}
 }
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,22 +1,22 @@
 package server
 
 import (
-    "../../pkg/controllers"
-    "fmt"
-    "github.com/gorilla/mux"
-    "net/http"
+	"fmt"
+	"github.com/go-park-mail-ru/2019_1_OPG_plus_2/internal/pkg/controllers"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type Params struct {
-    Port string
+	Port string
 }
 
 func StartApp(params Params) error {
-    fmt.Println("Server starting at " + params.Port)
+	fmt.Println("Server starting at " + params.Port)
 
-    router := mux.NewRouter()
+	router := mux.NewRouter()
 
-    router.HandleFunc("/", controllers.MainHandler)
+	router.HandleFunc("/", controllers.MainHandler)
 
-	return http.ListenAndServe(":" + params.Port, router)
+	return http.ListenAndServe(":"+params.Port, router)
 }
