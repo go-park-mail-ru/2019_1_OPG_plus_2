03758940128b7da51d1adf8779e58877a5551385
DanikNik <sway9009>
2019-03-04 20:24:45 +0300

    swagger ui serving on route /docs/index.html now test swagger dummy api implemented gen_swaggerdoc.sh script written for generating docs easily

--- internal/pkg/controllers/server.go
+++ internal/pkg/controllers/server.go
@@ -1,19 +1,19 @@
 package controllers
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
-    defer func() {
-        if err := recover(); err != nil {
-            fmt.Println(err)
-        }
-    }()
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
 
-    _, err := fmt.Fprintf(w, "Site of OPG+2!")
-    if err != nil {
-        fmt.Println(err)
-    }
+	_, err := fmt.Fprintf(w, "Site of OPG+2!")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
