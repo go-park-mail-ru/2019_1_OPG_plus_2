937323abd2a5c20bcb390e30d9eef42e214af4e5
DanikNik <sway9009>
2019-04-25 13:14:46 +0300

    playing with travis

--- build/auth_run_container.sh
+++ build/auth_run_container.sh
@@ -9,4 +9,4 @@ then
 
 echo [SERVER-AUTH] The published port of the container is ${port}
 
-docker build --rm -t colors-back-auth -f auth.Dockerfile .. && docker run -d --network=opg-net -p ${port}:50242 --name colors-back-auth colors-back-auth >> ~/docker.log && echo [SERVER-AUTH] Server is now running at: ${port}
+docker build --rm -t colors-back-auth -f auth.Dockerfile .. && docker run -d -e COLORS_AUTH_MODE=$1--network=opg-net -p ${port}:50242 --name colors-back-auth colors-back-auth >> ~/docker.log && echo [SERVER-AUTH] Server is now running at: ${port}
