606698ae20bb6b07d5191335b8d189cf33d0949a
Vladimir <vsev.s@mail.ru>
2019-04-28 21:47:36 +0300

    build script added

---
+++ build/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+go build -o ${COLORSBUILD}../cmd/server/main ${COLORSBUILD}../cmd/server/main.go;
+go build -o ${COLORSBUILD}../cmd/auth/main ${COLORSBUILD}../cmd/auth/auth_server.go;
+go build -o ${COLORSBUILD}../cmd/chat/main ${COLORSBUILD}../cmd/chat/chat_server.go;
