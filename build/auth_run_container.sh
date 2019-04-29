e758e4f273b6e0fd7fc511fb767191e978efbe0e
DanikNik <sway9009>
2019-04-30 00:17:45 +0300

    production modes for every docker in setup

--- build/auth_run_container.sh
+++ build/auth_run_container.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -eq 1 ]]
-then
-    port=$1
-    else
-    port=50242
-    fi
+port=50242
+auth_mode="RUNTIME"
+colors_mode="IN_DOCKER_NET"
 
+while getopts 'm:tp:' flag; do
+  case "${flag}" in
+    t) auth_mode="TEST";;
+    p) port=${OPTARG};;
+    m) mode=${OPTARG}
+        case ${mode} in
+            dockernet) colors_mode="IN_DOCKER_NET";;
+            dockerdb) colors_mode="USE_DOCKER_DB";;
+            production) colors_mode="PRODUCTION";;
+        esac
+        ;;
+  esac
+done
+
+echo [MODE] ${colors_mode}
 echo [SERVER-AUTH] The published port of the container is ${port}
 
-docker build --rm -t colors-back-auth -f auth.Dockerfile .. && docker run -d -e COLORS_AUTH_MODE=$2 --network=opg-net -p ${port}:50242 --name colors-back-auth colors-back-auth >> ~/docker.log && echo [SERVER-AUTH] Server is now running at: ${port}
+docker build --rm -t colors-back-auth -f auth.Dockerfile .. &&\
+docker run -d -e COLORS_AUTH_MODE=${auth_mode} -e COLORS_SERVICE_USE_MODE=${colors_mode} --network=opg-net\
+ -p ${port}:50242 --name colors-back-auth colors-back-auth >> ~/docker.log\
+&& echo [SERVER-AUTH] Server is now running at: ${port}
