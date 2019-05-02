98786db6ec14483de103160eccf37b8cea0a621b
DanikNik <sway9009>
2019-05-02 16:31:35 +0300

    travis cd works

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,7 +5,7 @@ chmod 600 ./deploy_key
 ssh -i ./deploy_key $COLORS_MACHINE_USERNAME@$COLORS_MACHINE_ADDRESS << EOF
 
 cd go-park-mail-ru/2019_1_OPG_plus_2
-git checkout deploy
+git checkout dev
 git fetch
 cd build
 ./clear_up_containers.sh && ./initial && ./db_run_container.sh && ./auth_run_container.sh && ./server_run_container.sh
