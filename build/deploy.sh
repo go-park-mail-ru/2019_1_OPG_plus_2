3d241b52057783761ee3aff928ec0d518950d88b
DanikNik <sway9009>
2019-05-02 16:20:03 +0300

    testing travis cd

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ssh-keyscan -H $COLORS_MACHINE_ADDRESS >> ~/.ssh/known_hosts
+chmod 600 ./deploy_key
 ssh -i ./deploy_key $COLORS_MACHINE_USERNAME@$COLORS_MACHINE_ADDRESS
 
 cd go-park-mail-ru/2019_1_OPG_plus_2
