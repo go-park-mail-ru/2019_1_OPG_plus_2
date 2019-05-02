4c5e499fa62e7e3aaf139686d3675f5ff58e466a
DanikNik <sway9009>
2019-05-02 15:37:43 +0300

    testing travis cd

--- build/build.sh
+++ build/build.sh
@@ -3,3 +3,4 @@
 go build -o ${COLORSBUILD}../cmd/server/main ${COLORSBUILD}../cmd/server/main.go;
 go build -o ${COLORSBUILD}../cmd/auth/main ${COLORSBUILD}../cmd/auth/auth_server.go;
 go build -o ${COLORSBUILD}../cmd/chat/main ${COLORSBUILD}../cmd/chat/chat_server.go;
+
---
+++ build/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ssh -i ../deploy_key ${COLORS_MACHINE_USER}@${COLORS_MACHINE_ADDRESS}
+
+cd go-park-mail-ru/2019_1_OPG_plus_2
+cd build
+./db_run_container.sh && ./auth_run_container.sh && ./server_run_container.sh
+exit
