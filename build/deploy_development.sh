505095e4443023bc8257d8c523d4539e39b7bf3e
Vladimir Severov <vsev.s@mail.ru>
2019-05-07 00:58:27 +0300

    exports fixed

--- build/deploy_development.sh
+++ build/deploy_development.sh
@@ -3,6 +3,10 @@
 ssh-keyscan -H $DEVELOPMENT_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_development
 ssh -i ./deploy_key_development $DEVELOPMENT_MACHINE_USERNAME@$DEVELOPMENT_MACHINE_ADDRESS << EOF
+if [ -f ~/.bash_exports ]; then
+    . ~/.bash_exports
+fi
+
 cd go-park-mail-ru/2019_1_OPG_plus_2
 git checkout dev
 git pull
