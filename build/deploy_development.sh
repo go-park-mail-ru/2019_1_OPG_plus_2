048e3228a766abc485066fc2f4d3aadb5d830461
Vladimir Severov <vsev.s@mail.ru>
2019-05-07 00:56:11 +0300

    exports fixed

--- build/deploy_development.sh
+++ build/deploy_development.sh
@@ -3,11 +3,10 @@
 ssh-keyscan -H $DEVELOPMENT_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_development
 ssh -i ./deploy_key_development $DEVELOPMENT_MACHINE_USERNAME@$DEVELOPMENT_MACHINE_ADDRESS << EOF
-
 cd go-park-mail-ru/2019_1_OPG_plus_2
-git pull
 git checkout dev
 git pull
+
 cd build
 ./initial.sh
 ./clear_up_containers.sh -c
--- build/deploy_production.sh
+++ build/deploy_production.sh
@@ -3,12 +3,15 @@
 ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_production
 ssh -i ./deploy_key_production $PRODUCTION_MACHINE_USERNAME@$PRODUCTION_MACHINE_ADDRESS << EOF
+if [ -f ~/.bash_exports ]; then
+    . ~/.bash_exports
+fi
+
 cd back
-git pull
 git checkout production
 git pull
-cd build
 
+cd build
 ./initial.sh
 ./clear_up_containers.sh -c
 ./auth_run_container.sh -m production && ./server_run_container.sh -m production
