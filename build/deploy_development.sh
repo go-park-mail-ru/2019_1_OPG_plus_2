bb3912c83b2fc33501d1ade3a2cb70237b48881c
DanikNik <sway9009>
2019-05-06 12:37:03 +0300

    wtf is going on with travis

--- build/deploy_development.sh
+++ build/deploy_development.sh
@@ -4,8 +4,8 @@ ssh-keyscan -H $DEVELOPMENT_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_development
 ssh -i ./deploy_key_development $DEVELOPMENT_MACHINE_USERNAME@$DEVELOPMENT_MACHINE_ADDRESS << EOF
 cd go-park-mail-ru/2019_1_OPG_plus_2
-git checkout dev
-git fetch
+git checkout 50-travis-cd
+git pull
 cd build
 ./clear_up_containers.sh
 ./initial.sh
