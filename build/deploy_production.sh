77d24b3da98a6ea9f2c6e5379f878cce5de88bc3
DanikNik <sway9009>
2019-05-06 14:07:29 +0300

    all CD works correctly, minor bud fixes

--- build/deploy_production.sh
+++ build/deploy_production.sh
@@ -4,11 +4,12 @@ ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_production
 ssh -i ./deploy_key_production $PRODUCTION_MACHINE_USERNAME@$PRODUCTION_MACHINE_ADDRESS << EOF
 cd back
+git fetch
 git checkout production
 git fetch
 cd build
-./clear_up_containers.sh
 ./initial.sh
+./clear_up_containers.sh
 ./auth_run_container.sh -m production && ./server_run_container.sh -m production
 exit
 EOF
