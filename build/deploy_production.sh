d6766e9b8db587832d2c2a2fec6223e98b6e08d8
DanikNik <sway9009>
2019-05-06 14:56:12 +0300

    trying to deploy in production))

--- build/deploy_production.sh
+++ build/deploy_production.sh
@@ -4,11 +4,20 @@ ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deploy_key_production
 ssh -i ./deploy_key_production $PRODUCTION_MACHINE_USERNAME@$PRODUCTION_MACHINE_ADDRESS << EOF
 cd back
+git fetch
 git checkout production
 git fetch
 cd build
-./clear_up_containers.sh
+
 ./initial.sh
+./clear_up_containers.sh -c
+
+docker run --name "opg-net-proxyhost" \
+  --cap-add=NET_ADMIN --cap-add=NET_RAW \
+  --restart on-failure \
+  --net=opg-net --network-alias 'proxyhost' \
+  qoomon/docker-host >> ~/docker.log
+
 ./auth_run_container.sh -m production && ./server_run_container.sh -m production
 exit
 EOF
