e9ca2776418c3ea5475ff867adb9f20374543bcf
DanikNik <sway9009>
2019-05-08 04:59:23 +0300

    typo fix

--- internal/pkg/middleware/accessmonitoring.go
+++ internal/pkg/middleware/accessmonitoring.go
@@ -11,7 +11,6 @@ func AccessMonitoringMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		sw := NewStatusWriter(w)
 		next.ServeHTTP(sw, r)
-		fmt.Println(sw.Status)
 		monitoring.AccessCounter.WithLabelValues(
 			strconv.Itoa(sw.Status),
 			r.URL.String(),
